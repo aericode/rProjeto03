1913cdb72bcf0651f10b198f30ff77bdb3f5ff02
Eric Andrade <vloxflox@gmail.com>
2019-05-01 13:19:56 -0300

    plotter separado

---
+++ include/ray.h
@@ -0,0 +1,18 @@
+#ifndef RAYH
+#define RAYH
+#include "vec3.h"
+
+class ray
+{
+public:
+	ray();
+	ray(const vec3& a, const vec3& b);
+	vec3 origin() const;
+	vec3 direction() const;
+	vec3 point_at_parameter(float) const;
+
+	vec3 A;
+	vec3 B;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "vec3.h"
+
+using namespace std;
+
+int main(){
+	vec3 test;
+	test[0] = 1;
+	test[1] = 2;
+	test[2] = 3;
+	cout << test;
+}
